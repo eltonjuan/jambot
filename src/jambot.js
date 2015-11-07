d45bbc23ae0d78ee73e09e12fef05fe771a32287
Michael Amos <michael.amos2@nike.com>
2015-11-07 12:55:02 -0800

    added test route

--- src/jambot.js
+++ src/jambot.js
@@ -1,13 +1,14 @@
 import mongoose from 'mongoose';
 import express from 'express';
 import bodyParser from 'body-parser';
+import * as config from './config';
 
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 // boot the app
-app.listen(8080, () => {
-	console.log('jambot alive and listening on port: ');
+app.listen(config.PORT, (req, res) => {
+	console.log(`jambot alive and listening on port ${config.PORT}`);
+	res.send('OK');
 });
