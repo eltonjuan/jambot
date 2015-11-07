b3c79c4a4282676a290b906021da1a852e19478b
Michael Amos <michael.amos2@nike.com>
2015-11-07 15:42:40 -0800

    fixing lint

--- src/jambot.js
+++ src/jambot.js
@@ -1,35 +1,31 @@
 import Slack from 'slack-client';
 import mongoose from 'mongoose';
-import {Message} from './models/Message';
+// import {Message} from './models/Message';
 import {handler, reactor} from './MessageHandler';
 import * as config from './config';
-
 
 // start up jambot
 const slack = new Slack(config.SLACK_TOKEN, config.AUTO_RECONNECT, config.AUTO_MARK);
 
 slack.on('open', () => {
-	console.log(`Connected to Slack as @${slack.self.name}`)
+  console.log(`Connected to Slack as @${slack.self.name}`);
 });
 
-slack.on('message', (msg) => {
-	const parsed = handler(msg);
-	if (!parsed) { 
-		return;
-	};
+slack.on('message', msg => {
+  const parsed = handler(msg);
+  if (!parsed) { 
+    return;
+  }
 
-	// react to the message
-	reactor(parsed, slack);
+  // react to the message
+  reactor(parsed, slack);
 
-	// store the message
-	
-	
-
+  // store the message
 });
 
-slack.on('error', (err) => {
-	console.log('error connecting to slack');
-	throw new Error(err);
+slack.on('error', err => {
+  console.log('error connecting to slack');
+  throw new Error(err);
 });
 
 slack.login();
